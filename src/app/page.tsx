9dd5622fec2b0be24dc408f49f32dd1882e46731
mohammad.afshar <mohammad.afshar>
2025-04-16 09:53:50 +0330

    add shadcn ui table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import { SelectedRowsProvider } from "@/provides/selected-devices-row";
+import DevicesToAddTable from "./_components/table/table";
+import { ApiResponse } from "@/types/api-response.types";
+import { users } from "@/utils/mock-data";
+
+const data: ApiResponse = {
+  data: {
+    count: 9,
+    rows: users,
+  },
+};
+
 export default function Home() {
-  return <div className="">Home page</div>;
+  return (
+    <div className="">
+      <SelectedRowsProvider>
+        <DevicesToAddTable data={data} />
+      </SelectedRowsProvider>
+    </div>
+  );
 }
